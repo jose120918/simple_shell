204ae3013570965c3a02181db47855f6fa401410
jose120918 <jose.esteban0918@gmail.com>
2020-11-24 15:09:23 -0500

    Header file

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*-------- Librerias ----------*/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#endif
